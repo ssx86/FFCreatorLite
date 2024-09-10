7edda1cab68e18c80d307fe651dd3d664ff3ae7e
doom-9 <110001160+doom-9@users.noreply.github.com>
2024-09-10 14:34:36 +0800

    增加测试案例

--- examples/video.js
+++ examples/video.js
@@ -1,15 +1,12 @@
 const path = require('path');
 const colors = require('colors');
 const startAndListen = require('./listen');
-const { FFCreatorCenter, FFScene, FFImage, FFText, FFVideo, FFCreator } = require('../');
+const { FFCreatorCenter, FFScene, FFImage, FFText, FFVideo, FFCreator, FFGif } = require('../');
 
 const createFFTask = () => {
-  const img3 = path.join(__dirname, './assets/imgs/06.png');
-  const logo = path.join(__dirname, './assets/imgs/logo/logo2.png');
+  const img3 = path.join(__dirname, './assets/imgs/image2.gif');
   const font = path.join(__dirname, './assets/font/scsf.ttf');
-  const audio = path.join(__dirname, './assets/audio/03.wav');
-  const video1 = path.join(__dirname, './assets/video/video1.mp4');
-  const video2 = path.join(__dirname, './assets/video/video2.mp4');
+  const video1 = path.join(__dirname, './assets/video/video3.mp4');
 
   const cacheDir = path.join(__dirname, './cache/');
   const outputDir = path.join(__dirname, './output/');
@@ -18,30 +15,36 @@ const createFFTask = () => {
   const creator = new FFCreator({
     cacheDir,
     outputDir,
-    width: 576,
-    height: 1024,
+    width: 1920,
+    height: 1080,
     log: true,
     //audio,
   });
 
   // create FFScene
   const scene1 = new FFScene();
-  const scene2 = new FFScene();
-  scene1.setBgColor('#9980fa');
-  scene2.setBgColor('#ea2228');
 
-  const fvideo1 = new FFVideo({ path: video1, audio: true, y: 330 });
-  fvideo1.setScale(0.6);
+  const fvideo1 = new FFVideo({
+    path: video1,
+    audio: true,
+    clipStartTime: 0,
+    clipEndTime: 3,
+    appearTime: 0,
+    duration: 3,
+  });
   scene1.addChild(fvideo1);
 
-  const fvideo2 = new FFVideo({ path: video2, audio: false,  x: 300, y: 330 });
-  fvideo2.setScale(0.3);
-  fvideo2.addEffect('moveInRight', 2.5, 3.5);
+  const fvideo2 = new FFVideo({
+    path: video1,
+    audio: true,
+    clipStartTime: 3,
+    clipEndTime: 3.001,
+    appearTime: 3,
+    duration: 6,
+  });
   scene1.addChild(fvideo2);
 
-  const fimg3 = new FFImage({ path: img3, x: 60, y: 600 });
-  fimg3.setScale(0.4);
-  fimg3.addEffect('rotateInBig', 2.5, 1.5);
+  const fimg3 = new FFGif({ path: img3, x: 100, y: 100, appearTime: 3, duration: 6 });
   scene1.addChild(fimg3);
 
   const text1 = new FFText({ text: 'FFVideo案例', font, x: 140, y: 100, fontSize: 42 });
@@ -50,7 +53,17 @@ const createFFTask = () => {
   //text1.addEffect('fadeIn', 2, 1);
   scene1.addChild(text1);
 
-  scene1.setDuration(17);
+  const fvideo3 = new FFVideo({
+    path: video1,
+    audio: true,
+    clipStartTime: 3.001,
+    clipEndTime: 12,
+    appearTime: 6,
+    duration: 15,
+  });
+  scene1.addChild(fvideo3);
+
+  scene1.setDuration(15);
   creator.addChild(scene1);
 
   creator.start();
